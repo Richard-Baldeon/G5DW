7117bf996c7a9999cf7114c780c9edc66c9428a8
Alvaro Molina <amolinaeffio@gmail.com>
2026-07-05 11:00:14 -0500

    Se crea los servicios http para obtener la lista de catalogo por sap y por texto

--- arca-inventory-web/src/app/services/project.ts
+++ arca-inventory-web/src/app/services/project.ts
@@ -1,6 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Project {}
+export class Project {
+
+  constructor(
+    private readonly http: HttpClient
+  ){}
+
+  catalogo_sap(param: String){
+    return this.http.get(
+      'https://h6120959l8.execute-api.us-east-1.amazonaws.com/v1/catalogosap?material=' + param,
+      {responseType: 'json'}
+    );
+  }
+
+  catalogo_texto(param: String){
+    return this.http.get(
+      'https://h6120959l8.execute-api.us-east-1.amazonaws.com/v1/catalogotexto?descripcion_sap=' + param,
+      {responseType: 'json'}
+    );
+  }
+}
