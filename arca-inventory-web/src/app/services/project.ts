61cd62429e66298d22df1381604172c364665a2a
Alvaro Molina <amolinaeffio@gmail.com>
2026-07-05 19:01:02 -0500

    Se agrega el servicio para obtener los elementos

--- arca-inventory-web/src/app/services/project.ts
+++ arca-inventory-web/src/app/services/project.ts
@@ -23,4 +23,12 @@ export class Project {
       {responseType: 'json'}
     );
   }
+
+  elementos_maquina(param: String){
+    return this.http.get(
+      'https://h6120959l8.execute-api.us-east-1.amazonaws.com/v1/getelementos?maquina=' + param,
+      {responseType: 'json'}
+    );
+  }
+
 }
