87c50833f08c6bf8aae59e237e8ccc7d5f09ebbe
Alvaro Molina <amolinaeffio@gmail.com>
2026-07-05 20:31:07 -0500

    Se crea un nuevo modelo de variables

---
+++ arca-inventory-web/src/app/core/models/repuesto_maquina.model.ts
@@ -0,0 +1,11 @@
+export interface RepuestoMaquina {
+  ELEMENTO: string;
+  DESCRIPCION_BREVE: string;
+  DESCRIPCION_EXTENSA: string;
+  NOMBRE_TECNICO: string;
+  MARCA: string;
+  NUMERO_PARTE: string;
+  CODIGO_SAP: string;
+  ENLACE_IMAGEN: string;
+  TIENE_FOTO?: boolean;
+}
