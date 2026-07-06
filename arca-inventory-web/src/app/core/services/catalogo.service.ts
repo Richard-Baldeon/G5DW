4c04c7f556dca416a2ce10905ef628dea4982797
LuisLobato18 <luislobatomedina@gmail.com>
2026-07-06 14:11:43 -0500

    Update catalogo.service.ts

--- arca-inventory-web/src/app/core/services/catalogo.service.ts
+++ arca-inventory-web/src/app/core/services/catalogo.service.ts
@@ -7,7 +7,7 @@ import { Repuesto } from '../models/repuesto.model';
   providedIn: 'root'
 })
 export class CatalogoService {
-  private readonly dataUrl = 'assets/data/repuestos.json';
+  private readonly apiUrl = 'https://staging.d3v26duzwdz1ba.amplifyapp.com/home';
 
   constructor(private http: HttpClient) {}
 
