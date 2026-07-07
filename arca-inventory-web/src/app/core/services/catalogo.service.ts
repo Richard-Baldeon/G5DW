55dd089e6189c9f8fa2573fb23d03b8b508afb52
LuisLobato18 <luislobatomedina@gmail.com>
2026-07-07 02:43:05 -0500

    Update catalogo.service.ts

--- arca-inventory-web/src/app/core/services/catalogo.service.ts
+++ arca-inventory-web/src/app/core/services/catalogo.service.ts
@@ -7,7 +7,7 @@ import { Repuesto } from '../models/repuesto.model';
   providedIn: 'root'
 })
 export class CatalogoService {
-  private readonly apiUrl = 'https://ltofrwe28.execute-api.us-east-1.amazonaws.com/prod/repuestos';  
+  private readonly apiUrl = 'https://ltyofrwe28.execute-api.us-east-1.amazonaws.com/prod/repuestos';  
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Repuesto[]> {
